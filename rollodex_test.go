91cc40e1bf6b4d340c56b95adeb89ca38005aee9
Sam Lewis <sam.vr.lewis@gmail.com>
2021-05-11 19:50:57 +1000

    test: Add a very basic rollodex test

---
+++ rollodex_test.go
@@ -0,0 +1,33 @@
+package meshboi
+
+import (
+	"bytes"
+	"net"
+	"testing"
+	"time"
+)
+
+func TestRollodex(t *testing.T) {
+	conn, _ := net.ListenUDP("udp", &net.UDPAddr{IP: net.ParseIP("127.0.0.1"), Port: 33333})
+	rollo, err := NewRollodex(conn, 1*time.Second, 5*time.Second)
+
+	if err != nil {
+		t.FailNow()
+	}
+
+	go rollo.Run()
+
+	client, err := net.Dial("udp", "127.0.0.1:33333")
+
+	client.Write([]byte(`{"networkName": "test"}`))
+
+	time.Sleep(2 * time.Second)
+
+	buf := make([]byte, 1000)
+
+	client.Read(buf)
+
+	if !bytes.Contains(buf, []byte("127.0.0.1")) {
+		t.Fatalf("Didn't get back expected IP")
+	}
+}
