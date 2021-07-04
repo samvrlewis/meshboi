3bfa4b14026962ef18d61fd343a1862c6a4395c8
Sam Lewis <sam.vr.lewis@gmail.com>
2021-07-04 13:00:47 +1000

    cmd/meshboi: Don't set default vpn-ip
    Not that useful to have a default and marking it as not default better
    signals that it is something that needs to be set.

--- cmd/meshboi/main.go
+++ cmd/meshboi/main.go
@@ -24,7 +24,7 @@ func main() {
 	networkName := clientCommand.String("network", "", "The unique network name that identifies the mesh (should be the same on all members in the mesh)")
 	tunName := clientCommand.String("tun-name", "tun", "The name to assign to the tun adapter")
 	tunMtu := clientCommand.Int("tun-mtu", 1200, "The MTU of the tun")
-	vpnIPPrefixString := clientCommand.String("vpn-ip", "192.168.50.2/24", "The IP address (with subnet) to assign to the tunnel")
+	vpnIPPrefixString := clientCommand.String("vpn-ip", "", "The IP address (with subnet) to assign to the tunnel eg: 192.168.50.1/24")
 	rolodexAddr := clientCommand.String("rolodex-address", "rolodex.samlewis.me", "The IP address of the meshboi server")
 	rolodexPort := clientCommand.Int("rolodex-port", defaultPort, "The port of the server")
 	psk := clientCommand.String("psk", "", "The pre shared key to use (should be the same on all members in the mesh)")
@@ -52,6 +52,12 @@ func main() {
 
 		if *networkName == "" {
 			log.Error("network argument not set.")
+			clientCommand.PrintDefaults()
+			os.Exit(1)
+		}
+
+		if *vpnIPPrefixString == "" {
+			log.Error("vpn-ip argument not set.")
 			clientCommand.PrintDefaults()
 			os.Exit(1)
 		}
